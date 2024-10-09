235c312a76269f65b1329329df77aa53b4740712
bogdanharchenko <balabuha493@gmail.com>
2024-10-09 13:42:43 +0300

    Add new app again

--- src/app/MyApp.java
+++ src/app/MyApp.java
@@ -3,5 +3,6 @@ package app;
 public class MyApp {
     public static void main(String[] args){
         System.out.println("This is shop-online app");
+        System.out.println("Shop-online for customers");
     }
 }
